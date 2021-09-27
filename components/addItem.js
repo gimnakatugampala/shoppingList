34a28b423ca452f34e16ee5879668a2419e43c15
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-27 23:59:19 +0530

    add task

---
+++ components/addItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text ,TouchableOpacity ,TextInput } from 'react-native'
+
+export default function addItem() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
